cd120dcc429acba1e5127f104800804f32a03ecb
XoDus <XoDus@DESKTOP-F3SR3MH>
2022-10-30 20:55:55 -0400

    Docs/Comments Finalized for current build

--- equationfunction.py
+++ equationfunction.py
@@ -3,7 +3,11 @@ import math
 
 
 def generateEquation(difficulty=1):
-
+    """
+        Takes a difficulty and randomly generates a question
+        Based off the difficulty the problem will consist of different forms of Arithmetic
+        returns a question formatted as {number, operator, number}
+    """
     global operator
 
     value1 = r.randrange(int(math.pow(difficulty, 2)), 10 * difficulty + int(math.pow(difficulty, 2)))
@@ -22,7 +26,10 @@ def generateEquation(difficulty=1):
 
 
 def generateEquationList(size, difficulty=1):
-
+    """
+        Constructs a list of equations based off the range of size variable
+        Returns the equation list
+    """
     eqnList = []
 
     if size > 0 and difficulty >= 1:
@@ -34,7 +41,11 @@ def generateEquationList(size, difficulty=1):
 
 
 def formatEquationList(eqnlist):
-
+    """
+        Takes the equation list and formats it based off the number of elements it has
+        Formats it to be {'number operator number' = value}
+        returns the formatted list
+    """
     formattedEqnList = "List of {} equations:\n\n".format(len(eqnlist))
 
     for equation in eqnlist:
@@ -43,7 +54,12 @@ def formatEquationList(eqnlist):
 
 
 def quizFromEqnList(eqnlist):
-
+    """
+        Prompts the user to complete a set amount of questions based on the size of the equation list
+        Compares the user input to the correct answer and if it is correct increments the amount correct
+        If the input is wrong the quiz continues and the user is given the correct score
+        Upon completion of the quiz the user is given their percentage of questions answered correctly
+    """
     amntCorrect = 0
     qCount = 1
 
@@ -58,7 +74,6 @@ def quizFromEqnList(eqnlist):
 
     print("\nTotal Score: {}/{} ({}%)".format(amntCorrect, len(eqnlist), int(100 * (amntCorrect / len(eqnlist)))))
 
-print(generateEquation(10))
-#print(generateEquationList(1, 4))   # <----- The list of question/answer tuples, for connecting with GUI
+#print(generateEquation(10))
+#print(generateEquationList(3, 4))   # <----- The list of question/answer tuples, for connecting with GUI
 #quizFromEqnList(generateEquationList(3, 4))    # <----- Text only output test
-
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pygame
 import random
+from equationfunction import quizFromEqnList, generateEquationList
 
 pygame.init()
 
 DEBUGMODE = True
 
+# Sets the bounds of the GUI
 screenW = 800
 screenH = 600
 screen = pygame.display.set_mode((screenW, screenH))
 pygame.display.set_caption('Mars Mathematical Materials')
-
+# Assigns information to user sprite
 playerImg = pygame.image.load('sprites\player.png')
 initialPlayerX = 375
 initialPlayerY = 80
@@ -19,30 +21,42 @@ playerX_change = 0
 playerY_change = 0
 playerMoveSpeed = 3
 playerSideToCheck = "D"
-
+# Assigns information to question sprite
 questionImg = pygame.image.load('sprites\question.png')
 
 
 def text(contents, x, y, font="Arial", size=20):
+    # Displays text based off a determined X and Y coordinate
     textcontent = pygame.font.SysFont(font, size).render(contents, 1, (0, 0, 0))
     screen.blit(textcontent, (x, y))
 
 
 def maze(filepath, x, y, widthScale, heightScale):
+    # Takes the maze image, converts it to desired width and height, places it at an assigned X and Y coordinate
     imp = pygame.image.load(filepath).convert()
     imp = pygame.transform.scale(imp, (imp.get_width() * widthScale, imp.get_height() * heightScale))
     screen.blit(imp, (x, y))
 
 
 def player(x, y):
+    # Takes an image and assigns it to the user sprite placed at an assigned X and Y coordinate
     screen.blit(playerImg, (x, y))
 
 
 def question(contents, x, y):
+    # Assigns the math equation to a sprite placed at an assigned X and Y coordinate
     screen.blit(questionImg, (x, y))
 
 
 def wallAtPlayerSide(side):
+    """
+        Takes which direction the player sprite is moving
+        Based off which way, records the pixels touching the side of the sprite
+        Compares these recorded pixels to the defined colors if statement.
+        If the color is black, it says there's a collision and to stop the user from moving that direction
+        If the color is yellow, it identifies it as a question, prints questionImg Touched and removes the question sprite
+        if the color is white, collision is false, the player sprite can progress
+    """
     try:
         if playerSideToCheck == "U":
             color = screen.get_at((playerX, playerY - 3))
@@ -53,22 +67,22 @@ def wallAtPlayerSide(side):
             color = screen.get_at((playerX - 3, playerY))
             color2 = screen.get_at((playerX - 3, playerY + int(playerImg.get_height() / 3)))
             color3 = screen.get_at((playerX - 3, playerY + int(playerImg.get_height() / 1.5)))
-            color4 = screen.get_at((playerX - 3, playerY + playerImg.get_height() - 1))
+            color4 = screen.get_at((playerX - 3, playerY + int(playerImg.get_height() - 1)))
         elif playerSideToCheck == "D":
-            color = screen.get_at((playerX, playerY + 12))
-            color2 = screen.get_at((playerX + int(playerImg.get_width() / 3), playerY + 12))
-            color3 = screen.get_at((playerX + int(playerImg.get_width() / 1.5), playerY + 12))
-            color4 = screen.get_at((playerX + playerImg.get_width(), playerY + 12))
+            color = screen.get_at((playerX, playerY + int(playerImg.get_height() + 2)))
+            color2 = screen.get_at((playerX + int(playerImg.get_width() / 3), playerY + int(playerImg.get_height() + 2)))
+            color3 = screen.get_at((playerX + int(playerImg.get_width() / 1.5), playerY + int(playerImg.get_height() + 2)))
+            color4 = screen.get_at((playerX + int(playerImg.get_width() + 0), playerY + int(playerImg.get_height() + 2)))
         elif playerSideToCheck == "R":
-            color = screen.get_at((playerX + 13, playerY))
-            color2 = screen.get_at((playerX + 13, playerY + int(playerImg.get_height() / 3)))
-            color3 = screen.get_at((playerX + 13, playerY + int(playerImg.get_height() / 1.5)))
-            color4 = screen.get_at((playerX + 13, playerY + playerImg.get_height() - 1))
+            color = screen.get_at((playerX + int(playerImg.get_width() + 2), playerY))
+            color2 = screen.get_at((playerX + int(playerImg.get_width() + 2), playerY + int(playerImg.get_height() / 3)))
+            color3 = screen.get_at((playerX + int(playerImg.get_width() + 2), playerY + int(playerImg.get_height() / 1.5)))
+            color4 = screen.get_at((playerX + int(playerImg.get_width() + 2), playerY + int(playerImg.get_height() - 1)))
 
         if color == (0, 0, 0, 255) or color2 == (0, 0, 0, 255) or color3 == (0, 0, 0, 255) or color4 == (0, 0, 0, 255):
             return True
         if color == (255, 255, 0, 255) or color2 == (255, 255, 0, 255) or color3 == (255, 255, 0, 255) or color4 == (255, 255, 0, 255):
-            print("questionImg Touched")
+            print("{}".format(quizFromEqnList(generateEquationList(3, 4))))
             questionImg.fill((0, 0, 0, 0))
         elif color == (255, 255, 255, 255) and color2 == (255, 255, 255, 255) and color3 == (255, 255, 255, 255) and color4 == (255, 255, 255, 255):
             return False
@@ -82,6 +96,7 @@ paused = False
 running = True
 clock = pygame.time.Clock()
 while running:
+    # Main gameplay loop
 
     screen.fill((255, 155, 155))
 
@@ -89,48 +104,49 @@ while running:
 
     question("Test contents", 210, 200)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pygame.event.get():  # Records a keypress
+        if event.type == pygame.QUIT:  # Exits the game
             running = False
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:  # Activates debug mode
             if event.key == pygame.K_d:
                 DEBUGMODE = not DEBUGMODE
-            if event.key == pygame.K_ESCAPE:
+            if event.key == pygame.K_ESCAPE:  # Exits the game
                 running = False
-            if event.key == pygame.K_F11:
+            if event.key == pygame.K_F11:  # Toggles fullscreen for the game
                 pygame.display.toggle_fullscreen()
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE:  # Pauses and unpauses the instance
                 paused = not paused
                 print("paused?: ", paused)
-                if paused:
+                if paused:  # Locks user sprite movement
                     playerX_change = 0
                     playerY_change = 0
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT:  # Moves user sprite left if there is no collision or pause
                 playerX_change = 0
                 playerY_change = 0
                 playerSideToCheck = "L"
                 if not wallAtPlayerSide("L") and not paused:
                     playerX_change = -playerMoveSpeed
-            elif event.key == pygame.K_RIGHT:
+            elif event.key == pygame.K_RIGHT:  # Moves user sprite right if there is no collision or pause
                 playerX_change = 0
                 playerY_change = 0
                 playerSideToCheck = "R"
                 if not wallAtPlayerSide("R") and not paused:
                     playerX_change = playerMoveSpeed
-            elif event.key == pygame.K_UP:
+            elif event.key == pygame.K_UP:  # Moves user sprite up if there is no collision or pause
                 playerX_change = 0
                 playerY_change = 0
                 playerSideToCheck = "U"
                 if not wallAtPlayerSide("U") and not paused:
                     playerY_change = -playerMoveSpeed
-            elif event.key == pygame.K_DOWN:
+            elif event.key == pygame.K_DOWN:  # Moves user sprite down if there is no collision or pause
                 playerX_change = 0
                 playerY_change = 0
                 playerSideToCheck = "D"
                 if not wallAtPlayerSide("D") and not paused:
                     playerY_change = playerMoveSpeed
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+        if event.type == pygame.KEYUP:  # Stops user sprite movement when pressed button is released
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or \
+                    event.key == pygame.K_DOWN:
                 playerX_change = 0
                 playerY_change = 0
 
@@ -143,25 +159,27 @@ while running:
         playerX = 0
     elif playerX >= screenW - playerImg.get_width():
         playerX = screenW - playerImg.get_width()
-
     if playerY <= 0:
         playerY = 0
     if playerY >= screenH - playerImg.get_height():
         playerY = screenH - playerImg.get_height()
 
+    # Occurs when user sprite passes the finish line, places user sprite back at start
     if playerY >= 548:
         print("Done! {:.2f} seconds elapsed".format(pygame.time.get_ticks()/1000))
         playerX = initialPlayerX
         playerY = initialPlayerY
 
     player(playerX, playerY)
-    if DEBUGMODE:
+
+    if DEBUGMODE:   # Displays the debug menu when TRUE
         text("Press D to hide: x: {}, y: {}, x mov: {}, y mov: {}, side moving: {}, wall at side?: {}".format(playerX, playerY, playerX_change, playerY_change, playerSideToCheck, wallAtPlayerSide(playerSideToCheck)), 20, 10)
-    if paused:
+    if paused:      # Displays the pause menu when TRUE
         text("-PAUSED-", 50, 500, "Impact", 24)
+
     pygame.display.flip()
 
-    if wallAtPlayerSide(playerSideToCheck):
+    if wallAtPlayerSide(playerSideToCheck):     # Stops user sprite movement when recording collision data
         if playerSideToCheck == "L":
             playerX_change = 0
         elif playerSideToCheck == "R":
@@ -171,4 +189,4 @@ while running:
         elif playerSideToCheck == "D":
             playerY_change = 0
 
-    clock.tick(60)  # game runs at 60 FPS
+    clock.tick(60)  # game runs at 60 FPS, waits 1/60th of a second to restart the game's main loop
